eb30e97a2417bc1e7a106075e6271eb081dae492
Janghun <jang@huny.dev>
2025-05-17 23:14:07 +0900

    feat: Netlify Functions와 Go를 사용한 Hello API 및 데모 페이지 구현

---
+++ netlify/functions/hello/go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.24.1
+
+require github.com/aws/aws-lambda-go v1.48.0 // indirect
---
+++ netlify/functions/hello/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: `{"message": "안녕하세요! Netlify Functions with Go! 👋"}`,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
